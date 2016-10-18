38570bd5544e0b7dc66cff5711dd2b307b8a7913
chongbenben <154108162@qq.com>
2016-10-18 19:59:42 +0800

    第一次提交代码

---
+++ pages/liwushuo/fenlei/fenlei.js
@@ -0,0 +1,16 @@
+Page({
+  data: {
+    imgUrls: [
+      '/images/img1.jpg',
+      '/images/img2.png',
+      '/images/img3.jpg',
+      '/images/img4.jpg',
+      '/images/img2.png',
+      '/images/img4.jpg',
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 2000,
+    duration: 1000
+  },
+})
